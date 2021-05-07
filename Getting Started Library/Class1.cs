1bdd2bc2d7a97c9f0459594ac750941384c22569
Ethan <ethancharlesbell@gmail.com>
2021-05-06 21:15:50 -0400

    Implement first test

--- Getting Started Library/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Getting_Started_Library
-{
-    public class Class1
-    {
-    }
-}
---
+++ Getting Started Library/SumToN.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Getting_Started_Library
+{
+    public class SumToN
+    {
+        public static int SumToNSolution(int n)
+        {
+            return 0;
+        }
+    }
+}
