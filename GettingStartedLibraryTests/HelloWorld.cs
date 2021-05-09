f415c6d4f4eab0d57613aae29c41795f27726668
Ethan <ethancharlesbell@gmail.com>
2021-05-09 09:21:03 -0400

    reorganizing

---
+++ GettingStartedLibraryTests/HelloWorld.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Getting_Started_Library
+{
+    public class HelloWorld
+    {
+        public static string HelloWorldSolution()
+        {
+            return "Hello World";
+        }
+    }
+}
