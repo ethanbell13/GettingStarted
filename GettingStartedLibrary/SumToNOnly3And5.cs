53f4bf3772324c2d28729fc1d508cb8d8b77baca
Ethan <ethancharlesbell@gmail.com>
2021-05-10 17:16:31 -0400

    Create SumToN3And5 with test

---
+++ GettingStartedLibrary/SumToNOnly3And5.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace Getting_Started_Library
+{
+    public class SumtonOnly3And5 
+    {
+        public static int SumToNOnly3And5(int n)
+        /*takes an integer that is no less than 3 and outputs the sum of all multiples of 3 and 5 up to and including
+        the integer.*/
+        {
+            if (n < 3)
+            {
+                Console.WriteLine("Please enter a positive integer that is no less than 3 to receive a valid answer.");
+                int x = 1;
+                return x;
+            }
+            else
+            {
+                int x = 0;
+                for (int i = 1; i <= n; i++)
+                {
+                    if ((i % 3) = 0)
+                    {
+                        x += i
+                    }
+                    else if ((i % 5) = 0)
+                    {
+                        x += i
+                    }
+                }
+                return x;
+            }
+        }
+    }
+}
---
+++ GettingStartedLibraryTests/SumToNOnly3And5Test.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace Getting_Started_Library.Tests
+{
+    [TestClass]
+    public class SumToNOnly3And5Test
+    {
+        [TestMethod]
+        public void BasicTest()
+        {
+            int input = 10;
+            int result = SumtonOnly3And5.SumToNOnly3And5Solution(input);
+            Assert.AreEqual(result, 33);
+        }
+        [TestMethod]
+        public void LowIntTest()
+        {
+            int input = 2;
+            int result = SumtonOnly3And5.SumToNOnly3And5Solution(input);
+            Assert.AreEqual(result, 1);
+        }
+    }
+}
+
